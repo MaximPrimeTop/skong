74e117720968a4d81d9482e0463bb787cc0b0e86
MaximPrime <MaximPrime@DESKTOP-489MOVH>
2025-09-26 02:14:30 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("skong")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("skong")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("522a9512-9733-45b0-9a94-191c6bae6bfd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ skong.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GlobalEnums;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using InControl;
+namespace skong
+{
+    [BepInPlugin("MaximPrime.skong", "skong", "1.0.0")]
+    public class Skong : BaseUnityPlugin
+    {
+
+        internal static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("skong");
+
+        public void Awake()
+        {
+            Log.LogInfo("skonging really good");
+            Harmony.CreateAndPatchAll(typeof(Skong), null);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UberPostprocess), "OnRenderImage")]
+        public static bool OnRenderImagePatch(ref RenderTexture source, ref RenderTexture destination)
+        {
+            Graphics.Blit(source, destination);
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ControllerDetect), "ShowController")]
+        public static void ShowControllerPatch(ref GamepadType gamepadType)
+        {
+            Log.LogInfo("skonged the silk to the PS4 song");
+            gamepadType = GamepadType.PS4;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIButtonSkins), "GetButtonSkinFor", new Type[] { typeof(InputControlType)})]
+        public static void GetButtonSkinForPS4Patch(UIButtonSkins __instance)
+        {
+            try
+            {
+                var ihField = __instance.GetType().GetField("ih", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var inputHandler = (InputHandler)ihField.GetValue(__instance);
+                if (inputHandler != null)
+                {
+                    inputHandler.activeGamepadType = GamepadType.PS4;
+                    Log.LogInfo("mwahahah i changed to da ps4!!");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
